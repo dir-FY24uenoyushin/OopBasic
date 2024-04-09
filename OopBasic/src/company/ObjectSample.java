ed587b993cbaf1b8ef3971bbc1ee698c1feeb618
dir-FY24uenoyushin <yushin.ueno.x100@dirbato.co.jp>
2024-04-09 12:29:49 +0900

    変更

--- OopBasic/src/company/ObjectSample.java
+++ OopBasic/src/company/ObjectSample.java
@@ -4,25 +4,46 @@ public class ObjectSample {
 
     public static void main(String[] args) {
         // インスタンスの作成
-        Department department = new Department("営業部","xx",1000000);
-        Employee employee = new Employee("鈴木", department, "課長", 100);
+        Department salesDepartment = new Department("営業部","xx",1000000);
+        Employee sales = new Sales("鈴木", salesDepartment, "課長", 100);
 
         // インスタンスメソッドの呼び出し
-        employee.report();
-        employee.report(2);
-        employee.joinMeeting();
+        sales.report();
+        sales.report(2);
+        sales.joinMeeting();
 
         // １行空ける
         System.out.println("");
 
         // インスタンスの作成
         Department devDepartment = new Department("開発部","yy",0);
-        Employee engineer = new Employee("田中", devDepartment, "一般社員", ８８);
+        Engineer engineer = new Engineer("田中", devDepartment, "一般社員", ８８,"Java");
 
         // インスタンスメゾットの呼び出し
         engineer.report();
         engineer.joinMeeting();
+        engineer.developSoftware();
         
+        // ポリモーフィズムの確認
+        Employee projectManager = new Engineer("佐藤", devDepartment, "PM", 99, "Java");
+        
+        // インスタンスメソッドの呼び出し
+        projectManager.report();
+        projectManager.joinMeeting();
+        if (projectManager instanceof Engineer) {
+            // developSoftwareメソッドを呼び出す
+            ((Engineer) projectManager).developSoftware();
+             }
+        // 1行空ける
+        System.out.println("");
 
+        // アルバイトインスタンスの作成
+        ParttimeWorker parttimeWorker = new ParttimeWorker("太田", salesDepartment);
+        
+        // 実装したメソッドの呼び出し
+        ((Workable) sales).work();
+        ((Workable) engineer).work();
+        ((Workable) projectManager).work();
+        ((Workable) parttimeWorker).work();
     }
 }
